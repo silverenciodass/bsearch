fd204c815c0c1ff633af114c148b5a672677d5e2
Ulisses Cardoso <rasracapur@outlook.com>
2022-07-18 21:22:50 -0300

    modified:   src/bsearch/bsearch.hpp ----------------------------------------- add getters,setters,constructor and desconstructor in BSearch class

--- src/bsearch/bsearch.hpp
+++ src/bsearch/bsearch.hpp
@@ -1,6 +1,37 @@
 #ifndef   BSEARCH
 #define   BSEARCH
 
+#include <iostream>
+#include <vector>
+
+class BSearch
+{
+    private:
+        std::vector<int> array;
+        int operations;
+
+    public:
+
+        BSearch();
+        ~BSearch();
+        
+        std::vector<int> get_array() {
+	        return this->array;
+        }
+        
+        void set_array(std::vector<int> array) {
+	        this->array = array;
+        }
+        
+        int get_operations() {
+	        return this->operations;
+        }   
+        
+        void set_operations(int operations) {
+	        this->operations = operations;
+        }
+        
+};
 
 
 #endif // BSEARCH
